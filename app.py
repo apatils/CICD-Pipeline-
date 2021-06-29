25b21021d989e7e38fe5cbd73eab8589690bafe9
Akash Patil <patilakashsuresh@gmail.com>
2021-06-29 21:02:57 +0530

    updated return message

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route("/hello", methods=["GET"])
 def another_page():
-    return "Hello Python !!"
+    return "Hello DevOps !!"
 
 
 app.run('0.0.0.0', debug=True, port=5050)
