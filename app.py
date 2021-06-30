8b5989bc237e1e829599865e652baf088a49909d
Akash Patil <patilakashsuresh@gmail.com>
2021-06-30 17:53:16 +0530

    updated endpoint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/isEven", methods=['GET'])
+@app.route("/isOdd", methods=['GET'])
 def index():
     number = int(request.args.get("number"))    
     if number % 2 == 0:
@@ -13,7 +13,7 @@ def index():
 
 @app.route("/hello", methods=["GET"])
 def another_page():
-    return "Hello DevOps !!"
+    return "Hello World !!"
 
 
 app.run('0.0.0.0', debug=True, port=5050)
